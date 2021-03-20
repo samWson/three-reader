b7a7f1f7036d2fd68c955088d398fd64d31b8e60
Samuel Wilson <samWson@users.noreply.github.com>
2021-03-20 21:27:02 +1300

    Initial commit

---
+++ lib/three_reader.ex
@@ -0,0 +1,18 @@
+defmodule ThreeReader do
+  @moduledoc """
+  Documentation for `ThreeReader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ThreeReader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/three_reader_test.exs
@@ -0,0 +1,8 @@
+defmodule ThreeReaderTest do
+  use ExUnit.Case
+  doctest ThreeReader
+
+  test "greets the world" do
+    assert ThreeReader.hello() == :world
+  end
+end
