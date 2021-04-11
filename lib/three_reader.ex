7c9b4f4b8d184979aebcd4c3a733c9684214c7a2
Samuel Wilson <samWson@users.noreply.github.com>
2021-04-11 20:42:42 +1200

    Delete code generated from `mix new...`

--- lib/three_reader.ex
+++ lib/three_reader.ex
@@ -1,32 +1,17 @@
 defmodule ThreeReader do
-    use HTTPoison.Base
+  use HTTPoison.Base
 
   @moduledoc """
   Documentation for `ThreeReader`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ThreeReader.hello()
-      :world
-
-  """
-  def hello do
-    IO.puts("Hello world!")
-    :world
-  end
-
   def main(args) do
-      {:ok, response} = get_feed()
-      body = get_response_body(response)
-    hello()
+    {:ok, response} = get_feed()
+    body = get_response_body(response)
   end
 
   def get_feed() do
-	HTTPoison.get!("https://www.ruby-lang.org/en/feeds/news.rss")
+    HTTPoison.get!("https://www.ruby-lang.org/en/feeds/news.rss")
   end
 
   def get_response_body(response) do
--- test/three_reader_test.exs
+++ test/three_reader_test.exs
@@ -1,8 +1,4 @@
 defmodule ThreeReaderTest do
   use ExUnit.Case
   doctest ThreeReader
-
-  test "greets the world" do
-    assert ThreeReader.hello() == :world
-  end
 end
