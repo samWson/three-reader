dac8eb12f75c38b097f621864561aa07576a6461
Samuel Wilson <samWson@users.noreply.github.com>
2021-04-11 20:46:32 +1200

    Parse basic RSS XML in doctest
    For info: it's a good idea to place the `~S` sigil at the start
    of the docstring e.g.
    ```
    @doc ~S"""
    ...
    """
    ```
    
    It stops `\r\n` characters from being converted to carriage returns and
    newlines before the test is started. Without the sigil there was an
    error where the variable `xml` was being treated as a function that
    could not be found.
    
    https://elixir-lang.org/getting-started/sigils.html

--- lib/three_reader.ex
+++ lib/three_reader.ex
@@ -15,6 +15,22 @@ defmodule ThreeReader do
   end
 
   def get_response_body(response) do
+  end
 
+  @doc ~S"""
+  # parse_feed
+
+  Parses a string of XML into something useful.
+
+  ## Examples
+
+    iex> xml = "<rss version=\"2.0\">
+    ...></rss>"
+    "<rss version=\"2.0\">\n</rss>"
+    iex> ThreeReader.parse_feed(xml)
+    [%{attr: [version: "2.0"], name: :rss, value: []}]
+  """
+  def parse_feed(xml) do
+    Quinn.parse(xml)
   end
 end
